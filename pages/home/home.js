5369b786728794a1d8f843ab3951b123e7085ed1
ccomma <782453374@qq.com>
2022-06-27 09:16:25 +0800

    modify

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,16 +8,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    carouselA: {},
-    carouselList: [],
-    topCategoryList: []
+    carouselList: [{imageUrl:'https://imgcps.jd.com/ling4/100019386660/5Lqs6YCJ5aW96LSn/5L2g5YC85b6X5oul5pyJ/p-5f3a47329785549f6bc7a6f8/80c01a53/cr/s/q.jpg'},{imageUrl:'https://img14.360buyimg.com/pop/s1180x940_jfs/t1/125178/3/27426/83461/624e59efE96592896/c1a950159a19922e.jpg.webp'}],
+    categoryList: [{name:'首页'},{name:'家电'},{name:'数码'},{name:'食品饮料'},{name:'电脑办公'},{name:'个护清洁'},{name:'女装'},{name:'宠物'},{name:'爱车'},{name:'箱包工具'},{name:'男鞋'}],
+    modelList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   async onLoad(options) {
-    this.initData();
+    // this.initData();
   },
 
   /**
@@ -60,11 +60,10 @@ Page({
    */
   async initData() {
     const carouselList = await Carousel.getCarouselList();
-    const topCategoryList = await Category.getTopCategoryList();
+    const categoryList = await Category.getTopCategoryList();
     this.setData({
-      carouselA: carouselList[1],
       carouselList: carouselList,
-      topCategoryList: topCategoryList
+      categoryList: categoryList
     });
   }
 
